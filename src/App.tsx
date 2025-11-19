8b018fa2e3cf85266672a84358ffbbbceaf83d44
Allan518 <zhanganlun@gmail.com>
2025-11-19 00:48:07 -0600

    Fix env path and update admin login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom"
 import UserHome from "./pages/UserHome"
 import AdminLogin from "./pages/AdminLogin"
 import AdminHome from "./pages/AdminHome"
-import Requests from "./pages/Requests"
 import Inventory from "./pages/Inventory"
 import UserManagement from "./pages/UserManagement"
 import CheckedOut from "./pages/CheckedOut"
@@ -14,7 +13,6 @@ export default function App() {
       <Route path="/" element={<UserHome />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route path="/admin" element={<AdminHome />} />
-      <Route path="/admin/requests" element={<Requests />} />
       <Route path="/admin/inventory" element={<Inventory />} />
       <Route path="/admin/user-management" element={<UserManagement />} />
     </Routes>
